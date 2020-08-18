26f82662d3797eb9b803426d8396eac62021aca4
Neil Stoker <neil.stoker@gmail.com>
2020-08-18 15:23:12 +0100

    Added logging #justBecause

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nstoker/pluralsight_code-school-on-track-with-golang
 
 go 1.14
+
+require github.com/sirupsen/logrus v1.6.0
--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logrus.Infof("Starting up")
 	if len(os.Args) > 1 {
 		fmt.Println(os.Args[1])
 	} else {
 		fmt.Println("Hello, I am Gopher!")
 	}
+
+	logrus.Infof("Normal exit")
 }
